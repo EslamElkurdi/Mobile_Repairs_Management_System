6fe3c6281fb7fd102b26fac7cc7e804eb4bb2486
ESLAM <elkurdieslam@gmail.com>
2022-12-30 11:02:45 +0200

    Create Class Function

---
+++ Mobile Repairs Management System/Functions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Repairs_Management_System
+{
+    class Functions
+    {
+
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string ConStr;
+
+
+
+    }
+}
